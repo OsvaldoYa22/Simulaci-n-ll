03df94820933dea4cc2c5fe7b1bfa63ff7a233e1
Osvaldo Yañez <100394404+OsvaldoYa22@users.noreply.github.com>
2022-06-17 12:25:28 -0500

    Add files via upload

---
+++ Tareas/T_10.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import warnings
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+from scipy import stats
+plt.style.use('seaborn')
+# plt.style.use('seaborn-colorblind') #alternativa
+plt.rcParams['figure.figsize'] = [8, 4.5]
+plt.rcParams['figure.dpi'] = 300
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+RISKY_ASSET = 'TSLA'
+START_DATE = '2015-01-01'
+END_DATE = '2022-01-01'
+df = yf.download(RISKY_ASSET, start=START_DATE, end=END_DATE, adjusted=True)
+print(f'Descargados {df.shape[0]} renglones de datos.')
+print(df)
+
+adj_close = df['Adj Close']
+returns = adj_close.pct_change().dropna()   
+
+ax = returns.plot()
+ax.set_title(f'{RISKY_ASSET} rendimientos: {START_DATE} - {END_DATE}', 
+             fontsize=16)
+
+
+plt.tight_layout()
+#plt.savefig('imagen1.png')
+plt.show()
+
+print(f'Rendimiento promedio: {100 * returns.mean():.2f}%')
